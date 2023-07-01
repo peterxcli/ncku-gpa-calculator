bdf6bfe8bf4131f9a63f0235f7631940dfc7d090
peterxcli <peterxcli@gmail.com>
2023-07-02 04:03:16 +0800

    add seo content

--- src/pages/[year]/[semester].tsx
+++ src/pages/[year]/[semester].tsx
@@ -5,6 +5,7 @@ import { useAppStore } from '@/store/store';
 import { calculateGPA } from '@/utils/calculate';
 import { v4 as uuidv4 } from 'uuid';
 import styles from './index.module.scss'
+import Head from 'next/head';
 
 const CoursePage = () => {
     const router = useRouter();
@@ -40,81 +41,85 @@ const CoursePage = () => {
     }, []);
 
     return (
-        <Container className={styles.container}>
-            <h2>{year}, {semester}</h2>
-            {
-                records[year as string]?.[semester as string]?.map((course, index) => (
-                    <div className={styles['form-group']} key={index}>
-                        <TextField
-                            className={styles.textfield}
-                            label='Course Name'
-                            value={course.name}
-                            onChange={(e) => updateCourse(year as string, semester as string, course.id, { ...course, name: e.target.value })}
-                        />
-                        <TextField
-                            className={styles.textfield}
-                            label='Score'
-                            value={course.score}
-                            onChange={(e) => updateCourse(year as string, semester as string, course.id, { ...course, score: Number(e.target.value) })}
-                            type="number"
-                        />
-                        <TextField
-                            className={styles.textfield}
-                            label='Credit'
-                            value={course.credit}
-                            onChange={(e) => updateCourse(year as string, semester as string, course.id, { ...course, credit: Number(e.target.value) })}
-                            type="number"
-                        />
-                        <Button color="error" onClick={() => removeCourse(year as string, semester as string, course.id)}>Delete</Button>
-                    </div>
-                ))
-            }
-            <form onSubmit={handleSubmit} className={styles['form-group']}>
-                <TextField
-                    className={styles.textfield}
-                    label='Course Name'
-                    value={courseName}
-                    onChange={(e) => setCourseName(e.target.value)}
-                    placeholder="Course Name" required
-                />
-                <TextField
-                    className={styles.textfield}
-                    label='Score'
-                    value={score}
-                    onChange={(e) => setScore(Number(e.target.value))}
-                    placeholder="Score" required type="number"
-                />
-                <TextField
-                    className={styles.textfield}
-                    label='Credit'
-                    value={credit}
-                    onChange={(e) => setCredit(Number(e.target.value))}
-                    placeholder="Credit"
-                    required
-                    type="number"
-                />
-                <Button type="submit">Add Course</Button>
-            </form>
-            <div className={styles['nav-btn']}>
-                <Button
-                    className={styles['ctl-btn']}
-                    onClick={() => setGpa(calculateGPA(records[year as string][semester as string]))}
-                    variant='contained'
-                >
-                    Calculate
-                </Button>
-                {gpa >= 0 && <h2 className={styles['ctl-btn']}>GPA: {gpa}</h2>}
-                <Button
-                    className={styles['ctl-btn']}
-                    onClick={() => router.push('/')}
-                >
-                    Back
-                </Button>
-            </div>
+        <>
+            <Head>
+                <title>成功大學 GPA 計算工具 | National Cheng Kung University GPA Calculator</title>
+                <meta name="description" content="提供一個能夠在裝置上永久儲存你的成績資料的網站的成功大學 GPA 計算工具，選擇學期，只需輸入你的成績，工具將快速計算你的 GPA。此 GPA 計算機使用104年後的新標準計算，請特別注意 | Offering a National Cheng Kung University GPA calculator that allows you to permanently save your grade data on your device. Choose the semester, simply input your grades, and the tool will quickly calculate your GPA. Please note that this GPA calculator uses the new standard calculation after 2015." />
+            </Head>
+            <Container className={styles.container}>
+                <h2>{year}, {semester}</h2>
+                {
+                    records[year as string]?.[semester as string]?.map((course, index) => (
+                        <div className={styles['form-group']} key={index}>
+                            <TextField
+                                className={styles.textfield}
+                                label='Course Name'
+                                value={course.name}
+                                onChange={(e) => updateCourse(year as string, semester as string, course.id, { ...course, name: e.target.value })}
+                            />
+                            <TextField
+                                className={styles.textfield}
+                                label='Score'
+                                value={course.score}
+                                onChange={(e) => updateCourse(year as string, semester as string, course.id, { ...course, score: Number(e.target.value) })}
+                                type="number"
+                            />
+                            <TextField
+                                className={styles.textfield}
+                                label='Credit'
+                                value={course.credit}
+                                onChange={(e) => updateCourse(year as string, semester as string, course.id, { ...course, credit: Number(e.target.value) })}
+                                type="number"
+                            />
+                            <Button color="error" onClick={() => removeCourse(year as string, semester as string, course.id)}>Delete</Button>
+                        </div>
+                    ))
+                }
+                <form onSubmit={handleSubmit} className={styles['form-group']}>
+                    <TextField
+                        className={styles.textfield}
+                        label='Course Name'
+                        value={courseName}
+                        onChange={(e) => setCourseName(e.target.value)}
+                        placeholder="Course Name" required
+                    />
+                    <TextField
+                        className={styles.textfield}
+                        label='Score'
+                        value={score}
+                        onChange={(e) => setScore(Number(e.target.value))}
+                        placeholder="Score" required type="number"
+                    />
+                    <TextField
+                        className={styles.textfield}
+                        label='Credit'
+                        value={credit}
+                        onChange={(e) => setCredit(Number(e.target.value))}
+                        placeholder="Credit"
+                        required
+                        type="number"
+                    />
+                    <Button type="submit">Add Course</Button>
+                </form>
+                <div className={styles['nav-btn']}>
+                    <Button
+                        className={styles['ctl-btn']}
+                        onClick={() => setGpa(calculateGPA(records[year as string][semester as string]))}
+                        variant='contained'
+                    >
+                        Calculate
+                    </Button>
+                    {gpa >= 0 && <h2 className={styles['ctl-btn']}>GPA: {gpa}</h2>}
+                    <Button
+                        className={styles['ctl-btn']}
+                        onClick={() => router.push('/')}
+                    >
+                        Back
+                    </Button>
+                </div>
+            </Container>
+        </>
 
-            
-
-        </Container>
     );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import { makeStyles } from '@mui/styles';
 import { useAppStore } from '@/store/store';
 import { v4 as uuidv4 } from 'uuid';
+import Head from 'next/head';
 
 const IndexPage = () => {
   const uuid = uuidv4();
@@ -22,72 +23,83 @@ const IndexPage = () => {
   };
 
   return (
-    <Container>
-      <h1>Calculate NCKU GPA</h1>
-      {
-        Array.from(Object.entries(records)).map(_year => (
-          Array.from(Object.entries(_year[1])).map(_semester => (
-            <div key={uuidv4()}>
-              <form onSubmit={() => handleEdit(_year[0], _semester[0])}>
-                <Select
-                  disabled
-                  value={_year[0]}
-                  MenuProps={{
-                    PaperProps: {
-                      style: {
-                        maxHeight: '200px',  // Change the height as per your need
+    <>
+      <Head>
+        <title>成功大學 GPA 計算工具 | National Cheng Kung University GPA Calculator</title>
+        <meta name="description" content="提供一個能夠在裝置上永久儲存你的成績資料的網站的成功大學 GPA 計算工具，選擇學期，只需輸入你的成績，工具將快速計算你的 GPA。此 GPA 計算機使用104年後的新標準計算，請特別注意 | Offering a National Cheng Kung University GPA calculator that allows you to permanently save your grade data on your device. Choose the semester, simply input your grades, and the tool will quickly calculate your GPA. Please note that this GPA calculator uses the new standard calculation after 2015." />
+      </Head>
+      <Container>
+        <h1>Calculate NCKU GPA</h1>
+        {
+          Array.from(Object.entries(records)).map(_year => (
+            Array.from(Object.entries(_year[1])).map(_semester => (
+              <div key={uuidv4()}>
+                <form onSubmit={() => handleEdit(_year[0], _semester[0])}>
+                  <Select
+                    disabled
+                    value={_year[0]}
+                    MenuProps={{
+                      PaperProps: {
+                        style: {
+                          maxHeight: '200px',  // Change the height as per your need
+                        },
                       },
-                    },
-                  }}
-                >
-                  {[...Array(20)].map((_, i) => (
-                    <MenuItem key={uuidv4()} value={new Date().getFullYear() - i}>
-                      {new Date().getFullYear() - i}
-                    </MenuItem>
-                  ))}
-                </Select>
+                    }}
+                  >
+                    {[...Array(20)].map((_, i) => (
+                      <MenuItem key={uuidv4()} value={new Date().getFullYear() - i}>
+                        {new Date().getFullYear() - i}
+                      </MenuItem>
+                    ))}
+                  </Select>
 
-                <Select disabled value={_semester[0]}>
-                  <MenuItem value='Spring'>Spring</MenuItem>
-                  <MenuItem value='Fall'>Fall</MenuItem>
-                </Select>
+                  <Select disabled value={_semester[0]}>
+                    <MenuItem value='Spring'>Spring</MenuItem>
+                    <MenuItem value='Fall'>Fall</MenuItem>
+                  </Select>
 
-                <Button type="submit">Edit</Button>
-              </form>
-            </div>
+                  <Button type="submit">Edit</Button>
+                </form>
+              </div>
+            )
+            ))
           )
-          ))
-        )
-      }
+        }
 
-      <form onSubmit={handleSubmit}>
-        <Select
-          value={year}
-          onChange={(e: SelectChangeEvent<number>) => setYear(e.target.value as number)}
-          MenuProps={{
-            PaperProps: {
-              style: {
-                maxHeight: '200px',  // Change the height as per your need
+        <form onSubmit={handleSubmit}>
+          <Select
+            value={year}
+            onChange={(e: SelectChangeEvent<number>) => setYear(e.target.value as number)}
+            MenuProps={{
+              PaperProps: {
+                style: {
+                  maxHeight: '200px',  // Change the height as per your need
+                },
               },
-            },
-          }}
-        >
-          {[...Array(20)].map((_, i) => (
-            <MenuItem key={i} value={new Date().getFullYear() - i}>
-              {new Date().getFullYear() - i}
-            </MenuItem>
-          ))}
-        </Select>
+            }}
+          >
+            {[...Array(20)].map((_, i) => (
+              <MenuItem key={i} value={new Date().getFullYear() - i}>
+                {new Date().getFullYear() - i}
+              </MenuItem>
+            ))}
+          </Select>
 
-        <Select value={semester} onChange={(e) => setSemester(e.target.value as string)}>
-          <MenuItem value='Spring'>Spring</MenuItem>
-          <MenuItem value='Fall'>Fall</MenuItem>
-        </Select>
+          <Select value={semester} onChange={(e) => setSemester(e.target.value as string)}>
+            <MenuItem value='Spring'>Spring</MenuItem>
+            <MenuItem value='Fall'>Fall</MenuItem>
+          </Select>
 
-        <Button type="submit">Select</Button>
-      </form>
+          <Button type="submit">Select</Button>
+        </form>
+        <h1>成功大學 GPA 計算工具 | National Cheng Kung University GPA Calculator</h1>
+        <p>提供一個能夠在裝置上永久儲存你的成績資料的網站的成功大學 GPA 計算工具，選擇學期，只需輸入你的成績，工具將快速計算你的 GPA。此 GPA 計算機使用104年後的新標準計算，請特別注意。</p>
+        <p>Offering a National Cheng Kung University GPA calculator that allows you to permanently save your grade data on your device. Choose the semester, simply input your grades, and the tool will quickly calculate your GPA. Please note that this GPA calculator uses the new standard calculation after 2015.</p>
+        <br/>
+        <p>加這一大長串是想順便玩一下 seo ㄟ嘿</p>
 
-    </Container>
+      </Container>
+    </>
   );
 };
 
