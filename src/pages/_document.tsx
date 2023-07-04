ccb70fbde1265b8686a592bdc41763b4b979db35
peterxcli <peterxcli@gmail.com>
2023-07-04 21:22:15 +0800

    add GA script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -6,6 +7,16 @@ export default function Document() {
       <Head />
       <body>
         <Main />
+        {/* look: https://nextjs.org/docs/messages/next-script-for-ga */}
+        <Script src='https://www.googletagmanager.com/gtag/js?id=G-HP4XL8HL98' />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HP4XL8HL98');
+          `}
+        </Script>
         <NextScript />
       </body>
     </Html>
